653ae4f0562015b1e105edffa812dd784fd0f74b
cfinn7789 <cfinn7789@gmail.com>
2023-08-03 18:24:56 -0400

    updated port

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
 
 const db = mysql.createConnection(
     {
       host: 'localhost',
+      port: '3306',
       user: 'root',
       password: 'root',
       database: 'employees_db'
